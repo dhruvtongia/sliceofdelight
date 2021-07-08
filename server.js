f6e73e445e57106db01dc860d58b6b939a7528e3
dhruvtongia <dhruvtongia2000@gmail.com>
2021-07-08 12:09:49 +0530

    fixed issue in server.js

--- server.js
+++ server.js
@@ -55,10 +55,6 @@ app.set('eventEmitter',eventEmitter);
 //-----------middlewares--------------//
 const adminAuth=require('./middlewares/adminAuth');
 app.use(express.urlencoded({extended:true}));
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials:true,
-}));
 app.use(express.json());
 
 
