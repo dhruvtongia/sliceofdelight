3ba465fdd2c252a68a031100fa268f55d0183319
dhruvtongia <dhruvtongia2000@gmail.com>
2021-07-08 12:05:11 +0530

    fixed issue in server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ eventEmitter.on('orderUpdated',(data)=>{
 if(process.env.NODE_ENV==="production")
 {
     app.use(express.static('client/build'));
-    const path=requie("path");
+    const path=require("path");
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     });
